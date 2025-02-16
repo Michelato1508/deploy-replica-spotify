a569eae285a045e1b9f139717af042d7f2a64b9a
Michelato1508 <mmichelato1508@gmail.com>
2025-02-15 21:07:33 -0300

    Projeto Final

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -2,7 +2,7 @@ import "dotenv/config";
 import axios from "axios";
 
 // const { NODE_ENV } = process.env;
-const URL = "http://localhost:3000";
+const URL = "https://deploy-replica-spotify.onrender.com";
 
 const responseArtist = await axios.get(`${URL}/artists`);
 const responseSong = await axios.get(`${URL}/songs`);
